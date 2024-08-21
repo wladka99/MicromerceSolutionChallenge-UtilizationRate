ce324b7dc0c18ab1481c936926a6fc3bc794068f
Kevin Voß <kevin.voss@micromerce.com>
2024-08-21 14:30:28 +0200

    adjust position of tableData

--- src/table-script.tsx
+++ src/table-script.tsx
@@ -7,38 +7,38 @@ import { useMemo } from "react";
 import sourceData from "./source-data.json";
 import type { SourceDataType, TableDataType } from "./types";
 
+/**
+ * Example of how a tableData object should be structured.
+ *
+ * Each `row` object has the following properties:
+ * @prop {string} person - The full name of the employee.
+ * @prop {number} past12Months - The value for the past 12 months.
+ * @prop {number} y2d - The year-to-date value.
+ * @prop {number} may - The value for May.
+ * @prop {number} june - The value for June.
+ * @prop {number} july - The value for July.
+ * @prop {number} netEarningsPrevMonth - The net earnings for the previous month.
+ */
+
+const tableData: TableDataType[] = (
+  sourceData as unknown as SourceDataType[]
+).map((dataRow, index) => {
+  const person = `${dataRow?.employees?.firstname} - ...`;
+
+  const row = {
+    person: `${person}`,
+    past12Months: `past12Months ${index} placeholder`,
+    y2d: `y2d ${index} placeholder`,
+    may: `may ${index} placeholder`,
+    june: `june ${index} placeholder`,
+    july: `july ${index} placeholder`,
+    netEarningsPrevMonth: `netEarningsPrevMonth ${index} placeholder`,
+  };
+
+  return row;
+});
+
 const Example = () => {
-  /**
-   * Example of how a tableData object should be structured.
-   *
-   * Each `row` object has the following properties:
-   * @prop {string} person - The full name of the employee.
-   * @prop {number} past12Months - The value for the past 12 months.
-   * @prop {number} y2d - The year-to-date value.
-   * @prop {number} may - The value for May.
-   * @prop {number} june - The value for June.
-   * @prop {number} july - The value for July.
-   * @prop {number} netEarningsPrevMonth - The net earnings for the previous month.
-   */
-
-  const tableData: TableDataType[] = (
-    sourceData as unknown as SourceDataType[]
-  ).map((dataRow, index) => {
-    const person = `${dataRow?.employees?.firstname} - ...`;
-
-    const row = {
-      person: `${person}`,
-      past12Months: `past12Months ${index} placeholder`,
-      y2d: `y2d ${index} placeholder`,
-      may: `may ${index} placeholder`,
-      june: `june ${index} placeholder`,
-      july: `july ${index} placeholder`,
-      netEarningsPrevMonth: `netEarningsPrevMonth ${index} placeholder`,
-    };
-
-    return row;
-  });
-
   const columns = useMemo<MRT_ColumnDef<TableDataType>[]>(
     () => [
       {
